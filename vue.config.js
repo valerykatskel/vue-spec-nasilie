046d0ff068443a75a9e78c7d320e8d17796dc8ce
Valery Katskel <ne.vse.srazu@gmail.com>
2019-08-28 16:15:42 +0300

    spa prerender plugin added

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+const path = require('path') 
+const PrerenderSPAPlugin = 
+require('prerender-spa-plugin')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? './'
-    : '/'
+    : '/',
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [ '/' ],
+      })
+    ]
+  }
 }
